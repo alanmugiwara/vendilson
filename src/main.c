f1424b0bc2b2df14081988f72f1df2dc712c8b35
Alan Cruz <alan_mugiwara@hotmail.com>
2024-10-25 14:39:55 -0300

    Implementação do bloco1

---
+++ src/main.c
@@ -0,0 +1,44 @@
+// O QUE SERÁ UTILIZADO
+
+// 1. Variáveis
+// 2. Biblioteca Standard Input / output
+// 3. Bilioteca Importa a biblioteca Standartt Librabry
+
+// ALGORITIMO
+// Passo 1 Inserir um Menu inicial
+// Passo 2 inserir valor de venda do produto
+// Passo 3 inserir valor de aquisição do produto
+// Passo 4 inserir taxa de comissão de venda
+
+// Passo 5 Calcular o valor da taxa em cima do valor do produto
+// Passo 6 calcular o valor de lucro (venda-custo-comissão)
+// Passo 7 Calcular o lucro em porcentagem
+// Passo 8 Calcular o valor final que será recebido pela venda
+
+// Passo 9 Mostrar o valor em dinheiro será descontado pela taxa de comissão
+// Passo 10 mostrar o valor de lucro em porcentagem 
+// Passo 11 mostrar o valor de lucro em dinheiro
+// Passo 12 mostrar o valor final em dinheiro que será recebido pela venda
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // Declaração de variáveis de input
+    float valor_venda, valor_aquisicao, porcent_taxa;
+
+    // Passo 1 | Menu inicial
+    printf("==============================\nBoas vindas ao Vendilson Jr!\n==============================\n\n");
+
+    // Passo 2 | Valor de venda
+    printf("Insira o valor de venda que deseja atribuir ao produto:\n");
+    scanf("%s", &valor_venda);
+
+    // Passo 3 | Valor de aquisição
+    printf("%s", &valor_aquisicao);
+
+    // Passo 4 | porcentagem da taxa de comsissão
+    pringf("")
+
+}
+
